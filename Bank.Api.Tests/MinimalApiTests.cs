e9be4b4b9f1ec1e03f3ce8d3fea051257584a550
PrivatePuma <lsantoro@students.ccu.edu>
2025-03-26 21:08:58 -0600

    Ready for assignment

--- Bank.Api.Tests/MinimalApiTests.cs
+++ Bank.Api.Tests/MinimalApiTests.cs
@@ -83,7 +83,5 @@ public class MinimalApiTests : IClassFixture<WebApplicationFactory<Program>>, ID
 
         var deletedAccount = _storage.GetAccount(accountId);
         deletedAccount.Should().BeNull();
-
-        // Test Commit
     }
 }
